94b1e9858839c7dadd5bb49b0bb7895039fcdbe2
Grzegorz Szermiński <szerman@poczta.fm>
2019-06-30 10:40:04 +0200

    Form validation - part 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ class App extends Component {
     username: "",
     email: "",
     pass: "",
-    accept: false
+    accept: false,
+
+    errors: {
+      username: true,
+      email: true,
+      pass: true,
+      accept: true
+    }
+  };
+
+  messages = {
+    username_incorrect:
+      " Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji.",
+    email_incorrect: " Brak @ w emial.",
+    password_incorrect: " Hasło musi mieć 8 znaków.",
+    accept_incorrect: " Niepotwierdzona zgoda."
   };
 
   handleChange = e => {
@@ -32,52 +47,63 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <form onSubmit={this.handleSubmit} noValidate>
-          <label htmlFor='user'>
+          <label htmlFor="user">
             Twoje imię:
             <input
-              type='text'
-              id='user'
-              name='username'
+              type="text"
+              id="user"
+              name="username"
               value={this.state.username}
               onChange={this.handleChange}
             />
+            {this.state.errors.username && (
+              <span>{this.messages.username_incorrect}</span>
+            )}
           </label>
 
-          <label htmlFor='email'>
+          <label htmlFor="email">
             Twój e-mail:
             <input
-              type='email'
-              id='email'
-              name='email'
+              type="email"
+              id="email"
+              name="email"
               value={this.state.email}
               onChange={this.handleChange}
             />
+            {this.state.errors.email && (
+              <span>{this.messages.email_incorrect}</span>
+            )}
           </label>
 
-          <label htmlFor='password'>
+          <label htmlFor="password">
             Twoje hasło:
             <input
-              type='password'
-              id='password'
-              name='pass'
+              type="password"
+              id="password"
+              name="pass"
               value={this.state.pass}
               onChange={this.handleChange}
             />
+            {this.state.errors.pass && (
+              <span>{this.messages.password_incorrect}</span>
+            )}
           </label>
 
-          <label htmlFor='accept'>
+          <label htmlFor="accept">
             <input
-              type='checkbox'
-              id='accept'
-              name='accept'
+              type="checkbox"
+              id="accept"
+              name="accept"
               checked={this.state.accept}
               onChange={this.handleChange}
             />
             Wyrażam zgodę wszelaką
           </label>
-
+          {this.state.errors.accept && (
+            <span>{this.messages.accept_incorrect}</span>
+          )}
           <button>Zapisz się</button>
         </form>
       </div>
