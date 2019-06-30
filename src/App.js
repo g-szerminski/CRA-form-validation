b82ded3e3e0703e4806897a758ab775890d610f9
Grzegorz Szermiński <szerman@poczta.fm>
2019-06-30 12:36:03 +0200

    Form validation - part 2

--- src/App.js
+++ src/App.js
@@ -42,7 +42,67 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log("działa");
+    const validation = this.formValidation();
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        pass: "",
+        accept: false,
+
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false
+        }
+      });
+      console.log("Formularz wysłany");
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          pass: !validation.password,
+          accept: !validation.accept
+        }
+      });
+    }
+  };
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (
+      this.state.username.length > 10 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      username = true;
+    }
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+    if (this.state.pass.length === 8) {
+      password = true;
+    }
+    if (this.state.accept) {
+      accept = true;
+    }
+    if (username && email && password && accept) {
+      correct = true;
+    }
+    return {
+      correct,
+      username,
+      email,
+      password,
+      accept
+    };
   };
 
   render() {
